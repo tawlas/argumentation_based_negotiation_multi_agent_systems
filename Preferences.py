4da13eca86e4988fd083017384392f0a283ac903
Alassane Watt <lastaw@alassanes-macbook-pro.home>
2021-04-10 21:51:33 +0200

    fin cours 4.1

--- Preferences.py
+++ Preferences.py
@@ -1,146 +1,104 @@
-#!/usr/bin/env python3
+from mesa import Model
+from mesa.time import RandomActivation
 import sys
-sys.path.append('D:\Documents\Cours\syst_multiagent\mesa_preference\mesa')
+sys.path.insert(0, ".")
+# sys.path.append('D:\Documents\Cours\syst_multiagent\mesa_preference\mesa')
+from communication.agent.CommunicatingAgent import CommunicatingAgent
+from communication.message.MessageService import MessageService
+
+from communication.preferences.Preferences import Preferences
 from communication.preferences.CriterionName import CriterionName
 from communication.preferences.CriterionValue import CriterionValue 
 from communication.preferences.Item import Item 
 from communication.preferences.Value import Value
 
-import numpy as np
+from random import randrange, shuffle
 
-class Preferences:
-    """Preferences class.
-    This class implements the preferences of an agent.
-
-    attr:
-        criterion_name_list: the list of criterion name (ordered by importance)
-        criterion_value_list: the list of criterion value
+class ArgumentAgent(CommunicatingAgent):
+    """ TestAgent which inherit from CommunicatingAgent.
     """
+    def __init__(self, unique_id, model, name):
+        super().__init__(unique_id, model, name)
+        self.preference = None
 
-    def __init__(self):
-        """Creates a new Preferences object.
-        """
-        self.__criterion_name_list = []
-        self.__criterion_value_list = []
+    def step(self):
+        super().step()
 
-    def get_criterion_name_list(self):
-        """Returns the list of criterion name.
-        """
-        return self.__criterion_name_list
+    def get_preference(self):
+        return self.preference
 
-    def get_criterion_value_list(self):
-        """Returns the list of criterion value.
-        """
-        return self.__criterion_value_list
-
-    def set_criterion_name_list(self, criterion_name_list):
-        """Sets the list of criterion name.
-        """
-        self.__criterion_name_list = criterion_name_list
-
-    def add_criterion_value(self, criterion_value):
-        """Adds a criterion value in the list.
-        """
-        self.__criterion_value_list.append(criterion_value)
-
-    def get_value(self, item, criterion_name):
-        """Gets the value for a given item and a given criterion name.
-        """
-        for value in self.__criterion_value_list:
-            if value.get_item() == item and value.get_criterion_name() == criterion_name:
-                return value.get_value()
-        return None
-
-    def is_preferred_criterion(self, criterion_name_1, criterion_name_2):
-        """Returns if a criterion 1 is preferred to the criterion 2.
-        """
-        for criterion_name in self.__criterion_name_list:
-            if criterion_name == criterion_name_1:
-                return True
-            if criterion_name == criterion_name_2:
-                return False
-
-    def is_preferred_item(self, item_1, item_2):
-        """Returns if the item 1 is preferred to the item 2.
-        """
-        return item_1.get_score(self) > item_2.get_score(self)
-
-    def most_preferred(self, item_list):
-        """Returns the most preferred item from a list.
-        """
+    def generate_preferences(self):
         # To be completed
-        scores = []
-        for item in item_list :
-            scores.append(item.get_score(self))
-        ind = np.argmax(scores)
-        best_item = item_list[ind]
-        
-        
-        return best_item
+        self.preference = Preferences()
+        self.preference.set_criterion_name_list(shuffle([CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT,
+                                                  CriterionName.CONSUMPTION, CriterionName.DURABILITY, CriterionName.NOISE]))
+        for it in self.model.get_list_items() :
+            for crit in self.preference.get_criterion_name_list() :
+                self.preference.add_criterion_value(CriterionValue(it, crit,Value(randrange(5))))
 
-    def is_item_among_top_10_percent(self, item, item_list):
-        """
-        Return whether a given item is among the top 10 percent of the preferred items.
+class ArgumentModel(Model):
 
-        :return: a boolean, True means that the item is among the favourite ones
-        """
-        # To be completed        
-        top_ten = []
-        num = int(np.ceil(len(item_list) / 10))
-        if num == 0 :
-            return False
-        for _ in range(num) :
-            it = self.most_preferred(item_list)
-            top_ten.append(it)
-            item_list.remove(it)
-            
-        if item in top_ten :
-            is_top_item = True
-        else :
-            is_top_item = False
-        return is_top_item
-
-
-if __name__ == '__main__':
-    """Testing the Preferences class.
+    """ ArgumentModel which inherit from Model.
     """
-    agent_pref = Preferences()
-    agent_pref.set_criterion_name_list([CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT,
-                                        CriterionName.CONSUMPTION, CriterionName.DURABILITY,
-                                        CriterionName.NOISE])
+    def __init__(self):
+        self.schedule = RandomActivation(self)
+        self.__messages_service = MessageService(self.schedule)
 
-    diesel_engine = Item("Diesel Engine", "A super cool diesel engine")
-    agent_pref.add_criterion_value(CriterionValue(diesel_engine, CriterionName.PRODUCTION_COST,
-                                                  Value.VERY_GOOD))
-    agent_pref.add_criterion_value(CriterionValue(diesel_engine, CriterionName.CONSUMPTION,
-                                                  Value.GOOD))
-    agent_pref.add_criterion_value(CriterionValue(diesel_engine, CriterionName.DURABILITY,
-                                                  Value.VERY_GOOD))
-    agent_pref.add_criterion_value(CriterionValue(diesel_engine, CriterionName.ENVIRONMENT_IMPACT,
-                                                  Value.VERY_BAD))
-    agent_pref.add_criterion_value(CriterionValue(diesel_engine, CriterionName.NOISE,
-                                                  Value.VERY_BAD))
+        # To be completed
+        
+        self.list_items = []
+        
+        a = ArgumentAgent(next(), self, "alice")
+        a.generate_preferences()
+        self.schedule.add(a)
+        
+        b = ArgumentAgent(next(), self, "bob")
+        b.generate_preferences()
+        self.schedule.add(b)
+        
+        self.running = True
 
-    electric_engine = Item("Electric Engine", "A very quiet engine")
-    agent_pref.add_criterion_value(CriterionValue(electric_engine, CriterionName.PRODUCTION_COST,
-                                                  Value.BAD))
-    agent_pref.add_criterion_value(CriterionValue(electric_engine, CriterionName.CONSUMPTION,
-                                                  Value.VERY_BAD))
-    agent_pref.add_criterion_value(CriterionValue(electric_engine, CriterionName.DURABILITY,
-                                                  Value.GOOD))
-    agent_pref.add_criterion_value(CriterionValue(electric_engine, CriterionName.ENVIRONMENT_IMPACT,
-                                                  Value.VERY_GOOD))
-    agent_pref.add_criterion_value(CriterionValue(electric_engine, CriterionName.NOISE,
-                                                  Value.VERY_GOOD))
+    def step(self):
+        self.__messages_service.dispatch_messages()
+        self.schedule.step()
+        
+    def get_list_items(self):
+        return self.list_items
 
-    """test list of preferences"""
-    print(diesel_engine)
-    print(electric_engine)
-    print(diesel_engine.get_value(agent_pref, CriterionName.PRODUCTION_COST))
-    print(agent_pref.is_preferred_criterion(CriterionName.CONSUMPTION, CriterionName.NOISE))
-    print('Electric Engine > Diesel Engine : {}'.format(agent_pref.is_preferred_item(electric_engine, diesel_engine)))
-    print('Diesel Engine > Electric Engine : {}'.format(agent_pref.is_preferred_item(diesel_engine, electric_engine)))
-    print('Electric Engine (for agent 1) = {}'.format(electric_engine.get_score(agent_pref)))
-    print('Diesel Engine (for agent 1) = {}'.format(diesel_engine.get_score(agent_pref)))
-    print('Most preferred item is : {}'.format(agent_pref.most_preferred([diesel_engine, electric_engine]).get_name()))
-    print('{} is in top ten : {}'.format(diesel_engine.get_name(), agent_pref.is_item_among_top_10_percent(diesel_engine, [diesel_engine, electric_engine])))
+
+if __name__ == "__main__":
+    argument_model = ArgumentModel()
+    # To be completed
+    agent0 = argument_model.schedule.agents[0]
+    agent1 = argument_model.schedule.agents[1]
+
+    assert(agent0.get_name() == "alice")
+    assert(agent1.get_name() == "bob")
+    print("*     get_name() => OK")
+    # list_items = argument_model.get_list_items()
+    # Agent1 to Agent2: PROPOSE (item)
+    msg_to_send = Message(agent0.get_name(), agent1.get_name(), MessagePerformative.PROPOSE, argument_model.get_list_items()[0])
+    agent0.send_message(msg_to_send)
+    new_msg = agent1.get_new_messages()[0]
+    perf = new_msg.get_performative()
+    item = new_msg.get_content()
+    
+    # item belongs to its 10% most preferred item
+    if agent1.preference.is_item_among_top_10_percent(item, argument_model.get_list_items()) :
+        # item is the most preferred item
+        most_prefered_item_1 = agent1.preference.most_preferred(argument_model.get_list_items())
+        if item == most_prefered_item_1:
+            # Agent2 to Agent1: ACCEPT (item)
+            accept_msg_1 = Message(agent1.get_name(), agent0.get_name(), MessagePerformative.ACCEPT, item)
+            agent1.send_message(accept_msg_1)
+            # Agent1 to Agent2: COMMIT (item)
+            commit_msg_0 = Message(agent0.get_name(), agent1.get_name(), MessagePerformative.COMMIT, item)
+            agent0.send_message(commit_msg_0)
+            # Agent2 to Agent1: COMMIT (item)
+            commit_msg_1 = Message(agent1.get_name(), agent0.get_name(), MessagePerformative.COMMIT, item)
+            agent1.send_message(commit_msg_1)
+        else:
+            propose_msg_1 = Message(agent1.get_name(), agent0.get_name(), MessagePerformative.PROPOSE, most_prefered_item_1)
+            agent1.send_message(propose_msg_1)
+    else :
+        agent1.send_message(Message("bob", "alice", MessagePerformative.ASK_WHY, item))
--- pw_argumentation.py
+++ pw_argumentation.py
@@ -1,21 +1,24 @@
-from mesa import Model
 from mesa.time import RandomActivation
-import sys
-sys.path.append('D:\Documents\Cours\syst_multiagent\mesa_preference\mesa')
+from mesa import Model
 from communication.agent.CommunicatingAgent import CommunicatingAgent
+from communication.message.MessagePerformative import MessagePerformative
 from communication.message.MessageService import MessageService
-
+from communication.message.Message import Message
 from communication.preferences.Preferences import Preferences
 from communication.preferences.CriterionName import CriterionName
-from communication.preferences.CriterionValue import CriterionValue 
-from communication.preferences.Item import Item 
 from communication.preferences.Value import Value
-
 from random import randrange, shuffle
+from communication.preferences.CriterionValue import CriterionValue
+from communication.preferences.Item import Item
+
+# import sys
+# sys.path.append('D:\Documents\Cours\syst_multiagent\mesa_preference\mesa')
+
 
 class ArgumentAgent(CommunicatingAgent):
     """ TestAgent which inherit from CommunicatingAgent.
     """
+
     def __init__(self, unique_id, model, name):
         super().__init__(unique_id, model, name)
         self.preference = None
@@ -30,37 +33,40 @@ class ArgumentAgent(CommunicatingAgent):
         # To be completed
         self.preference = Preferences()
         self.preference.set_criterion_name_list(shuffle([CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT,
-                                                  CriterionName.CONSUMPTION, CriterionName.DURABILITY, CriterionName.NOISE]))
-        for it in self.model.get_list_items() :
-            for crit in self.preference.get_criterion_name_list() :
-                self.preference.add_criterion_value(CriterionValue(it, crit,Value(randrange(5))))
-     
+                                                         CriterionName.CONSUMPTION, CriterionName.DURABILITY, CriterionName.NOISE]))
+        for it in self.model.get_list_items():
+            for crit in self.preference.get_criterion_name_list():
+                self.preference.add_criterion_value(
+                    CriterionValue(it, crit, Value(randrange(5))))
+
+
 class ArgumentModel(Model):
-    
+
     """ ArgumentModel which inherit from Model.
     """
+
     def __init__(self):
         self.schedule = RandomActivation(self)
         self.__messages_service = MessageService(self.schedule)
 
         # To be completed
-        
+
         self.list_items = []
-        
+
         a = ArgumentAgent(next(), self, "alice")
         a.generate_preferences()
         self.schedule.add(a)
-        
+
         b = ArgumentAgent(next(), self, "bob")
         b.generate_preferences()
         self.schedule.add(b)
-        
+
         self.running = True
 
     def step(self):
         self.__messages_service.dispatch_messages()
         self.schedule.step()
-        
+
     def get_list_items(self):
         return self.list_items
 
@@ -74,12 +80,41 @@ if __name__ == "__main__":
     assert(agent0.get_name() == "alice")
     assert(agent1.get_name() == "bob")
     print("*     get_name() => OK")
-
-    agent0.send_message(Message("alice", "bob", MessagePerformative.PROPOSE, argument_model.get_list_items()[0]))
+    # list_items = argument_model.get_list_items()
+    # Agent1 to Agent2: PROPOSE (item)
+    msg_to_send = Message(agent0.get_name(), agent1.get_name(
+    ), MessagePerformative.PROPOSE, argument_model.get_list_items()[0])
+    agent0.send_message(msg_to_send)
     new_msg = agent1.get_new_messages()[0]
     perf = new_msg.get_performative()
     item = new_msg.get_content()
-    if agent1.preference.is_item_among_top_10_percent(item, argument_model.get_list_items()) :
-        agent1.send_message(Message("bob", "alice", MessagePerformative.ACCEPT, item))
-    else :
-        agent1.send_message(Message("bob", "alice", MessagePerformative.ASK_WHY, item))
+
+    # item belongs to its 10% most preferred item
+    if agent1.preference.is_item_among_top_10_percent(item, argument_model.get_list_items()):
+        most_prefered_item_1 = agent1.preference.most_preferred(
+            argument_model.get_list_items())
+        # item is the most preferred item
+        if item == most_prefered_item_1:
+            # Agent2 to Agent1: ACCEPT (item)
+            accept_msg_1 = Message(
+                agent1.get_name(), agent0.get_name(), MessagePerformative.ACCEPT, item)
+            agent1.send_message(accept_msg_1)
+            # Agent1 to Agent2: COMMIT (item)
+            commit_msg_0 = Message(
+                agent0.get_name(), agent1.get_name(), MessagePerformative.COMMIT, item)
+            agent0.send_message(commit_msg_0)
+            # Agent2 to Agent1: COMMIT (item)
+            commit_msg_1 = Message(
+                agent1.get_name(), agent0.get_name(), MessagePerformative.COMMIT, item)
+            agent1.send_message(commit_msg_1)
+        else:
+            # Agent2 to Agent1: PROPOSE (item)
+            propose_msg_1 = Message(agent1.get_name(), agent0.get_name(
+            ), MessagePerformative.PROPOSE, most_prefered_item_1)
+            agent1.send_message(propose_msg_1)
+    else:
+        # Agent2 to Agent1: ASK_WHY (item)
+        askwhy_msg_1 = Message(
+            agent1.get_name(), agent0.get_name(), MessagePerformative.ASK_WHY, item)
+        agent1.send_message(askwhy_msg_1
+                            )
